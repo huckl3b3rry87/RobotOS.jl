8b191b9492344db0ccf514bfbfbb0195396fcd6b
Josh Langsfeld <jdlangs@gmail.com>
2016-08-01 18:17:58 -0400

    Minimal test case

--- test/segfault.jl
+++ test/segfault.jl
@@ -1,22 +1,10 @@
-using Base.Test
-using RobotOS
-using Compat
-RobotOS.debug(true)
+using PyCall
 
-init_node("jltest", anonymous=true)
+const rospy = pyimport("rospy")
+const gmsgs = pyimport("geometry_msgs.msg")
 
-@rosimport geometry_msgs.msg: PoseStamped, Vector3
-rostypegen()
+rospy[:init_node]("jltest", anonymous=true)
+jl_cb(msg::PyObject) = println("message: ", msg)
 
-using geometry_msgs.msg
-
-function pose_cb(msg)
-    println("message: ", msg)
-end
-
-const ros_pub = Publisher("vectors", Vector3; queue_size = 10)
-const ros_sub = Subscriber("poses", PoseStamped, pose_cb; queue_size = 10)
-
-rossleep(Duration(1.0))
-publish(ros_pub, Vector3(10,0,0))
-rossleep(Duration(1.0))
+const ros_sub = rospy[:Subscriber]("points", gmsgs["Point"], jl_cb)
+rospy[:sleep](10.0)
